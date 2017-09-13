1c9db1b68a1fa052d0ed47416b0d21cb4ce319ce
Doktor Schwein <doktorschwein@gmail.com>
2017-09-13 16:25:12 +0000

    Try fix for Heroku

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 
 console.log("test");
 
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
