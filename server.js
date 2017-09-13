027f581896b659e9cd123f7e63a6cded350f5b3e
Doktor Schwein <doktorschwein@gmail.com>
2017-09-13 18:26:47 +0000

    Add console.logs

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ const app = express();
 
 console.log("test");
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+// needed for Heroku (?)
+// app.set('port', (process.env.PORT || 5000))
+// app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+    console.log("get req:", req);
+    res.send('Hello World!')
 })
 
 // api.ai route
-
-app.post('/intent', (req, res) => { 
-    // .. ... ...
+app.post('/intent', (req, res) => {
     res.send('Hello')
 });
