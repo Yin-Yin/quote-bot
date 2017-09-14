c8048d42084c1b13d0773fe765e3107f6abf639f
Doktor Schwein <doktorschwein@gmail.com>
2017-09-14 16:48:59 +0000

    Trying code from heroku d start app

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ app.post('/intent', (req, res) => {
 });
 */
 
+
+/*
 var app = require('express').createServer();
 app.get('/',function(req,res) {
 	res.send("Hello World");
@@ -30,4 +32,26 @@ app.get('/',function(req,res) {
 
 app.listen(process.env.PORT || 3000,function(){
 	console.log("listening on 3000");
+});
+
+*/
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  //response.render('pages/index');
+  response.send("Hello World");
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
