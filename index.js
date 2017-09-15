f3d7710a4b7263570f5816fba6ebd2540771235d
Doktor Schwein <doktorschwein@gmail.com>
2017-09-15 16:12:36 +0000

    Trying with different syntax

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ var app = express();
 
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+//app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
-app.use(bodyParser.json())
+//app.use(bodyParser.json())
+
+app.use(express.bodyParser());
 
 
 app.set('port', (process.env.PORT || 5000));
