799286aaed0aa3472f013ad8e881d947cd968b9b
Doktor Schwein <doktorschwein@gmail.com>
2017-09-15 10:40:22 +0000

    Trying to display a text through api.ai

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-/*
+
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-*/
+
 
 app.get('/', function(req, res) {
   //response.render('pages/index');
@@ -19,7 +19,15 @@ app.get('/', function(req, res) {
 
 app.post('/intent', function(req, res){
   console.log("request: ", req);
-  res.send("Hello this is doge!");
+  res.send(
+    {
+"speech": "Barack Hussein Obama II was the 44th and current President of the United States.",
+"displayText": "Barack Hussein Obama II was the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
+"data": 'data',
+"contextOut": 'context out',
+"source": "DuckDuckGo"
+}
+    );
 })
 
 app.listen(app.get('port'), function() {
