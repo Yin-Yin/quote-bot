92757cf42ef4c42c8ebc5bfb0252e210abaacf8b
Doktor Schwein <doktorschwein@gmail.com>
2017-09-15 16:05:47 +0000

    Do I need to use the bodyparser to parse the body of a request?

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
-
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
