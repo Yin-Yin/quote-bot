ca4b9e1f4e6224da4cd7f8af0d8cb17ca1325e2b
Doktor Schwein <doktorschwein@gmail.com>
2017-09-16 09:43:57 +0000

    day is one off

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ function getZodiacSign(date) {
   // returns the zodiac sign according to day and month ( thanks to http://coursesweb.net/ for this elegant solution )
   var zodiac = ['', 'Capricorn', 'Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn'];
   var last_day = ['', 19, 18, 20, 20, 21, 21, 22, 22, 21, 22, 21, 20, 19];
-  return (day > last_day[month]) ? zodiac[month * 1 + 1] : zodiac[month];
+  return (day >= last_day[month]) ? zodiac[month * 1 + 1] : zodiac[month];
 
 
 
