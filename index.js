12e77678b1826b310d0a9638e729fbe2a83247f3
Doktor Schwein <doktorschwein@gmail.com>
2017-09-27 04:31:33 +0000

    Add readme file

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var bodyParser = require('body-parser')
+var bodyParser = require
 var express = require('express');
 var app = express();
 
@@ -21,7 +21,7 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
   //response.render('pages/index');
-  res.send("Hello World");
+  res.send('Hi there, this is the serve for the zodiac sign bot. For more info got to: <a href="https://github.com/Yin-Yin/zodiac-sign-bot/">Zodiac Sign Bot GitHub Page</a>');
 });
 
 app.post('/intent', function(req, res) {
@@ -29,10 +29,10 @@ app.post('/intent', function(req, res) {
   let actionIncomplete = req.body.result.actionIncomplete;
   let parameters = req.body.result.parameters;
   let intentName = req.body.result.metadata.intentName;
-  console.log("request: ", req.body);
-  console.log("date: ", parameters.date);
+  //console.log("request: ", req.body);
+  //console.log("date: ", parameters.date);
   let parameterDate = new Date(parameters.date);
-  console.log("parameterDate: ", parameterDate);
+  //console.log("parameterDate: ", parameterDate);
 
   let response = 'this is doge';
   if (intentName == 'zodiacsign.check') {
---
+++ readme.md
@@ -0,0 +1,7 @@
+_Zodiac Sign Bot_
+
+A simple bot that returns the zodiac sign for a date that you provide to the bot. Also gives you information about specific zodiac signs. 
+
+This is the webhook for API.ai. You can find a web demo of the bot here: https://bot.api.ai/zodiacsignbot
+
+You can talk to the bot on Telegram: _'@zodiacsignbot'_ and in kik, also _'zodiacsignbot'_
